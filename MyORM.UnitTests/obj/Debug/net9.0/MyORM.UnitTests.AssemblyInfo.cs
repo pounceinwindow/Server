fa03185aae28a0ac5491c1ba19e250fd9e7cf864
pounceinwindow <adk03042006@gmail.com>
2025-12-13 17:56:19 +0300

    local start

--- MyORM.UnitTests/obj/Debug/net9.0/MyORM.UnitTests.AssemblyInfo.cs
+++ MyORM.UnitTests/obj/Debug/net9.0/MyORM.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyORM.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b570ce97f4efd3c729c8ef99bb49e02efad972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b734665548f8b33e834cc7a967b78945f0dc22a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyORM.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyORM.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
